bacb328faab071b6e8bd03b847eedd3899747ff6
Mohamed Radwan <mohamed.radwan@cartologic.com>
2020-06-09 16:38:11 +0200

    Initialize with create-react-app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>Saudi map using Kepler</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
