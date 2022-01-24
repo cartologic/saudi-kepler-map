f26482a4fe1542ba7c3589f0e24001752fd06ed7
Muhammad Radwan <mohamed.radwan@cartologic.com>
2022-01-24 07:27:08 +0200

    Handle change in case formatting for data attributes

--- src/components/BarChartViewer/BarChart/BarChart.js
+++ src/components/BarChartViewer/BarChart/BarChart.js
@@ -12,27 +12,36 @@ class BarChart extends Component {
     let regionValues = [];
     // 1. Filter data by region
     regionValues = this.props.data.filter(
-      (d) => d.properties.RegionName_EN === regionName
+      (d) => {
+        return d.properties.RegionName_EN ? (
+          d.properties.RegionName_EN === regionName
+        ) : (
+          d.properties.regionname_en === regionName
+        )
+      } 
     );
+
+    // To fix the usually change of date property to lowercase/uppercase format.
+    const REPORTED_DATE = regionValues[0].properties.hasOwnProperty('Reportdt') ? 'Reportdt' : 'reportdt'
 
     // 2. Sorting filtered by region data
     regionValues = regionValues
       .slice()
       .sort((a, b) =>
-        d3.ascending(a.properties.Reportdt, b.properties.Reportdt)
+        d3.ascending(a.properties[REPORTED_DATE], b.properties[REPORTED_DATE])
       );
 
     // 3. Format the date to be yyyy-mm-dd instead of the current
     const labelDates = regionValues.map((d) =>
-      d.properties.Reportdt.slice(0, 10)
+      d.properties[REPORTED_DATE].slice(0, 10)
     );
-    regionValues.map((d, i) => (d.properties.Reportdt = labelDates[i]));
+    regionValues.map((d, i) => (d.properties[REPORTED_DATE] = labelDates[i]));
 
     // 4. Create total cases for each day. The output should be something like this:
     // date: Array of dates, totalCases: Array of cases as each case corresponding to each date index.
     let totalDatesCases = {
       dates: this.removeDuplicateDates(labelDates),
-      totalCases: this.generateValidCases(regionValues, this.props.caseType),
+      totalCases: this.generateValidCases(regionValues, this.props.caseType, REPORTED_DATE),
     };
     return totalDatesCases;
   };
@@ -52,15 +61,15 @@ class BarChart extends Component {
     return normalDates.concat(duplicateDates);
   };
 
-  generateValidCases = (data, caseType) => {
+  generateValidCases = (data, caseType, reportedDate) => {
     let validCases = [];
     let sumCasesforDate = 0;
 
     for (let i = 0; i < data.length - 1; i++) {
-      if (data[i + 1].properties.Reportdt !== data[i].properties.Reportdt) {
+      if (data[i + 1].properties[reportedDate] !== data[i].properties[reportedDate]) {
         if (
           data[i - 1] &&
-          data[i].properties.Reportdt === data[i - 1].properties.Reportdt
+          data[i].properties[reportedDate] === data[i - 1].properties[reportedDate]
         ) {
           sumCasesforDate = sumCasesforDate + data[i].properties[caseType];
           validCases.push(sumCasesforDate);
